84b129f69b76bfc651e94b47c19fca819dbbdc5e
Artem <artemmoskalenko07v1@gmail.com>
2025-09-19 16:26:34 +0200

    Task 3.1 done

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -3,40 +3,61 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+let wakeTime = 7;
+if (wakeTime == 7) {
   printOut("I can take the bus to school.");
+ } else if (wakeTime == 8) {
+  printOut("I can take  the train to school") 
+}
+else {printOut("I have to take a car to school");
 }
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let number = 0;
+ if (Number.isInteger(number)) {
+  printOut("Positiv");
+ }else {printOut("Negative");
+}
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 6 and 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let size1 = Math.random()*8
+printOut("Image size: " + size1.toFixed(1) + " MP"); 
+if (size1 <= 4) {printOut("Image is too small");
+} else if (size1 >= 6){printOut("Imge is too large");
+ } else {
+  printOut("Thank you!"); 
+}
 printOut(newLine);
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 8 + 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const monthList =["January", "February", "Mars", "April", "Mai",
+"Jun", "Juli", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+printOut(monthName);
+if (monthName.includes("r")){
+  printOut ("You must take vitamin D");
+} else {printOut ("You do not need vitamin D");
+}
+if (monthName == "February") {printOut("It is 28 days in February");
+} else if (monthName == "April" || monthName == "June" || monthName == "September" || monthName == "November") {
+  printOut("It is 30 days in " + monthName);
+}else {printOut("It is 31 days in " + monthName);}
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+if(monthName == "April") {
+  printOut ("We moved to the building next door due to refubishment in April.");
+}else if (monthName == "Mars" || monthName == "Mai") {
+  printOut ("Sorry, the galley is closed for refubishment in " + monthName + " waiting for you in June!");
+}else {printOut ("Welcome to the gallery!");
+}
 printOut(newLine);
