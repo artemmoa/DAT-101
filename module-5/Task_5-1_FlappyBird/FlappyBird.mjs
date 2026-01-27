34ee8a54297b4b09749289ef78ee7fc5ab40c341
Artem <artemmoskalenko07v1@gmail.com>
2026-01-27 12:38:06 +0100

    Flappy bird  school

--- module-5/Task_5-1_FlappyBird/FlappyBird.mjs
+++ module-5/Task_5-1_FlappyBird/FlappyBird.mjs
@@ -3,7 +3,9 @@
 import { TSpriteCanvas } from "libSprite";
 import { TBackground } from "./background.js";
 import { THero } from "./hero.js";
-
+import { TObstacle } from "./obstacle.js";
+import {TBait} from "./bait.js";
+import {TMenu} from "./menu.js";
 
 //--------------- Objects and Variables ----------------------------------//
 const chkMuteSound = document.getElementById("chkMuteSound");
@@ -29,19 +31,94 @@ const SpriteInfoList = {
   medal:        { x: 985 , y: 635 , width: 44   , height: 44  , count: 4  },
 };
 
-const EGameStatus = { idle: 0 };
+export const EGameStatus = {idle: 0, countDown: 1, gaming: 2, heroIsDead: 3, gameOver: 4,
+  state: 0};
 const background = new TBackground(spcvs, SpriteInfoList);
-const hero = new THero(spcvs, SpriteInfoList.hero1);
+export const hero = new THero(spcvs, SpriteInfoList.hero1);
+const obstacles = [];
+const baits = [];
+export const menu = new TMenu(spcvs, SpriteInfoList);
+let obstaclePassed = false;
+
 
 //--------------- Functions ----------------------------------------------//
+
+export function startGame(){
+  EGameStatus.state = EGameStatus.gaming;
+  setTimeout(spawnObstacle, 1000);
+  setTimeout(spawnBait,1000);
+}
+function spawnBait(){
+  if(EGameStatus.state === EGameStatus.gaming){
+  const bait = new TBait(spcvs, SpriteInfoList.food);
+  baits.push(bait);
+  const nextTime = Math.ceil(Math.random()*3)+1; 
+  setTimeout(spawnBait, nextTime*1000);
+  }
+}
+
+function spawnObstacle(){
+  if (EGameStatus.state === EGameStatus.gaming){
+  const obstacle = new TObstacle(spcvs, SpriteInfoList.obstacle);
+  obstacles.push(obstacle);
+  const nextTime = Math.ceil(Math.random() * 3) + 1;
+  setTimeout(spawnObstacle, nextTime * 1000);
+}
+}
+
 function animateGame(){
   hero.animate();
+  let eaten = -1;
+  for(let i = 0; i<baits.length; i++){
+    const bait = baits[i];
+    bait.animate();
+// console.log(bait.distanceTo(hero.center))
+    if(bait.distanceTo(hero.center)<20){
+      eaten = i;
+    }
+  }
+  if(eaten >=0){
+    console.log("eaten!");
+    baits.splice(eaten, 1);
+    hero.eat();
+  };
+  if (EGameStatus.state === EGameStatus.gaming){
+  background.animate();
+  let deleteObstacle = false;
+  for(let i = 0; i < obstacles.length; i++){
+    const obstacle = obstacles[i];
+    obstacle.animate();
+    if(obstacle.x < -50){
+      deleteObstacle = true;
+    }else if((obstacle.x + obstacle.width) < hero.x){
+      if(!obstaclePassed){
+        menu.incGameScore(1);
+        obstaclePassed = true;
+      }
+    }
+  }
+  if(deleteObstacle){
+    obstacles.splice(0,1);
+  }
+}
 }
 
 function drawGame(){
- background.draw();
- hero.draw();
+  background.drawBackground();
+
+  for(let i = 0; i < baits.length; i++){
+    const bait = baits[i];
+    bait.draw();
+  }
+  for(let i = 0; i < obstacles.length; i++){
+    const obstacle = obstacles[i];
+    obstacle.draw();
+  }
+  hero.draw();
+  background.drawGround();
+  menu.draw();
 }
+
 function loadGame() {
   console.log("Game Loaded");
   // Set canvas size to background size
@@ -50,9 +127,10 @@ function loadGame() {
 
   // Overload the spcvs draw function here!
   spcvs.onDraw = drawGame;
-  // Start animate engine
-  setInterval(animateGame, 10);        
 
+  //Start animate engine
+  setInterval(animateGame, 10);
+  
 } // end of loadGame
 
 
@@ -60,7 +138,10 @@ function onKeyDown(aEvent) {
   switch (aEvent.code) {
     case "Space":
       console.log("Space key pressed, flap the hero!");
-      hero.flap();
+      if(EGameStatus !== EGameStatus.heroIsDead){
+        hero.flap();
+      }
+      hero.flap();  
       break;
   }
 } // end of onKeyDown
